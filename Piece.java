71d39e9c2ea24273b40f668bc4b37876c22cc5ab
Louis-Charles Déziel <lcdeziel@gmail.com>
2019-03-13 23:40:23 -0400

    piece

--- Piece.java
+++ Piece.java
@@ -32,19 +32,12 @@ public void setCouleur ( String couleur )
   if (( couleur == "noir" ) || ( couleur == "blanc" ))
     this.couleur = couleur;
   }
- /* norme math�matique entre cette Position et une autre */
-/** 
-*m�thode permettant de calculer la norme math�matique entre deux Position 
-*depart  la position de d�part d'une Piece
-*arrivee la position d'arrivee d'Une Piece
-*la somme des carr�s des distances
-*
-*/
+
 public double norme (Position depart, Position arrivee)
   {
   return Math.pow((depart.getLigne()-arrivee.getLigne()), 2)+ Math.pow((depart.getColonne() - arrivee.getColonne()),2);
   }
-/* m�thode abstraite � impl�menter dans chacune des sous - classes */
+
 
 public abstract boolean estValide (Position depart, Position arrivee);
 
